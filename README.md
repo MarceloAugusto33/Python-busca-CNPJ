069c206c9de417165dd472bcfac34c728e6f915d
marcelo <sjsjsjmarcelo@gmail.com>
2024-02-28 19:26:07 -0300

    projeto completo

---
+++ README.md
@@ -0,0 +1,13 @@
+Opção 1: Script Python Busca CNPJ
+Objetivo: Codifique um script python que realize a consulta do CNPJ de uma empresa na API do receitaws. O script deve receber como entrada um cnpj e exibir na tela os dados no formtado do JSON abaixo:
+
+Requisitos
+O Script deve receber uma string via input (x)
+O Script deve tratar o input antes de fazer a requisição (x)
+O Script deve validar o input e somente realzar a requisição se o input estiver no formtado de CNPJ válido (x)
+O Script deve utilizar a biblioteca Python requests para realizar a requisição (x)
+O Script deve validar o status code da  (x)
+O Script deve validar a resposta da requisição e indicar se o CNPJ foi localizado na API ou não. (x)
+O Script deve exibir os dados no formato JSON acima (x)
+
+
---
+++ index.py
@@ -0,0 +1,88 @@
+import requests
+import re
+
+def display():
+    print("|                        |")
+    print("|  BUSCA DADOS POR CNPJ  |")
+    print("|                        |")
+    print("|  OPÇÕES                |")
+    print("|  |0| SAIR              |")
+    print("|  |1| BUSCAR POR CNPJ   |")
+    print("|                        |")
+
+def getApi(cnpj):
+
+    request = requests.get('https://receitaws.com.br/v1/cnpj/'+cnpj)
+
+    if(request.status_code == 200):
+
+        requestJSON = request.json()
+
+        if(requestJSON['status'] == 'ERROR'): 
+            return "Esse CNPJ não existe na api"
+        
+        print("Dados Encontrados na API")
+
+        result = {
+            "razao_social": requestJSON['nome'],
+            "codigo_atividade_principal": requestJSON['atividade_principal'][0]['code'],
+            "endereco": {
+                "numero": requestJSON['numero'],
+                "cep": requestJSON['cep'],
+                "municipio": requestJSON['municipio'],
+                "estado": requestJSON['uf'],
+            }
+
+        }
+
+        return result
+    else:
+        print("Failed request")
+
+def validaCNPJ(cnpj):
+    padrao_cnpj_regex = r'\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}'
+
+    if re.match(padrao_cnpj_regex, cnpj):
+        return True
+    else:
+        return False
+
+def filtrar_numeros(cnpj):
+    numeros = re.sub(r'\D', '', cnpj)
+    return str(numeros)
+
+
+while True:
+    display()
+    try:
+        option = int(input("Digite uma opção: "))
+
+        while option != 0 and option != 1:
+            print(" ")
+            option = int(input("Digite uma opção valida!: "))
+
+        if option == 0:
+            break
+        elif option == 1:
+            print("Digite o CNPJ NO MODELO SEGUINTE: '12.123.123/1234-12'")
+            cnpj = str(input("Digite o CNPJ: "))
+
+            if(validaCNPJ(cnpj)):
+                print(getApi(filtrar_numeros(cnpj)))
+            else: 
+                print("CNPJ INVALIDO!")
+                print("")
+    except ValueError:
+        print(" ")
+        print("DIGITE SOMENTE NUMEROS")
+        print(" ")
+
+        
+            
+
+
+    
+
+
+
+print("Programa finalizado!")
